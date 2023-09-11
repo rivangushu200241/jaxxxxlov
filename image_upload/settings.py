919a6b67523f4b11784f8ed34a71253c74857f1f
AMALGSOFT <rivangushu200241>
2023-09-11 09:03:32 +0300

    firt cim

--- image_upload/settings.py
+++ image_upload/settings.py
@@ -77,10 +77,20 @@ WSGI_APPLICATION = 'image_upload.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'NIwQ3QHz5OvSmQYHYyil',
+        'HOST': 'containers-us-west-125.railway.app',
+        'PORT': '7402',
     }
 }
 
