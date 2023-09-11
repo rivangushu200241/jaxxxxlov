1fae27d90da2a839145b42dfad34b77b0a5c2355
AMALGSOFT <rivangushu200241>
2023-09-11 11:28:51 +0300

    firt cim

--- image_upload/settings.py
+++ image_upload/settings.py
@@ -155,5 +155,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.environ.get('Demo_Email')
-EMAIL_HOST_PASSWORD = os.environ.get('Demo_Password')
+EMAIL_HOST_USER = os.environ.get('rivangushu@gmail.com')
+EMAIL_HOST_PASSWORD = os.environ.get('')
+# Demo_Password
